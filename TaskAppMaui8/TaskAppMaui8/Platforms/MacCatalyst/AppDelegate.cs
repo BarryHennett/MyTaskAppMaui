0962df15c8a0611ae6951d7f4b9780932aecdb85
BarryHennett <124011454+BarryHennett@users.noreply.github.com>
2023-10-26 16:40:33 +1300

    beginning

---
+++ TaskAppMaui8/TaskAppMaui8/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TaskAppMaui8
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
